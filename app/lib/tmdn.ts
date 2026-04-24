b82e89c7ee24946cd548ae88b0e48c97c2d29190
Jonathan Villanueva <jonathanvillanueva171@gmail.com>
2026-04-23 17:37:02 -0700

    api call to tmdb and rendering homepage with movies

--- app/lib/tmdn.ts
+++ app/lib/tmdn.ts
@@ -1,29 +1,28 @@
-import dotenv from "dotenv/config"
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY
+
 const TMDB_BASE_URL = "https://api.themoviedb.org/3"
 const options = {
     method: 'GET',
     headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${TMDB_API_KEY}`
+        Authorization: `Bearer ${TMDB_API_KEY}`,
     }
 }
 
 export async function fetchTmdbMovies(endpoint: string) {
     try {
         const response = await fetch(`${TMDB_BASE_URL}/${endpoint}`, options)
+        console.log("TMDB API Key:", TMDB_API_KEY)
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
         const data = await response.json()
         console.log(`Fetched ${endpoint}:`, data)
-        return data
+        return data.results
     } catch (error) {
         console.error(`Error fetching ${endpoint}:`, error)
     }
 }
-
 export function fetchPopularMovies() {
     return fetchTmdbMovies("movie/popular")
 }
