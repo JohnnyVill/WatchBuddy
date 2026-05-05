cb04b02245b33aa8c7438fe162e79093ac4c31f3
Jonathan Villanueva <jonathanvillanueva171@gmail.com>
2026-05-04 20:22:50 -0700

    password hashing implemented

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,6 +1,9 @@
 //connect to psql database using pg library
 import "server-only"
 import { Pool } from 'pg';
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 12;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL
@@ -10,15 +13,25 @@ export async function signup(username: string, password: string){
   const client = await pool.connect();
 
   try {
-    const result = await client.query(
-      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
-      [username, password]
+    const checkUser = await client.query(
+      "SELECT id FROM users WHERE username = $1",
+      [username]
     );
-    return result.rows[0].id;
-  } catch (error: any) {
-    if (error.code === "23505") {
+
+    if (checkUser.rows.length > 0) {
       throw new Error("USERNAME_EXISTS");
     }
+    else {
+      //password hashing
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+      const result = await client.query(
+        "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
+        [username, hashedPassword]
+      );
+      return result.rows[0].id;
+    }
+  } catch (error: any) {
     console.error("Error during signup:", error);
     throw error;
   }
